5b8a712315547b054ca02da3c0816e9ded4e1c52
ciklista <nitschke.jan@googlemail.com>
2020-07-02 18:05:59 +0200

    fixing log config path

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -8,7 +8,7 @@ def get_value_deserializer(data_input_format):
         return json.loads
 
 
-def get_logger(log_config_path='../config/logger_config.yml'):
+def get_logger(log_config_path='utils/logger_config.yml'):
     with open(log_config_path, 'r') as yaml_file:
         config = yaml.safe_load(yaml_file)
         logging.config.dictConfig(config)
