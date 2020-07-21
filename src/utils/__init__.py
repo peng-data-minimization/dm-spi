9609add4a2fc9dd11f1e8955013a2c8fe7dba710
Florian Nachtigall <florian.nachtigall@sap.com>
2020-07-21 20:28:08 +0200

    Support csv record processing

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,11 +1,22 @@
 import json
 import logging.config
 import yaml
+import csv
+from io import StringIO
 
 
 def get_value_deserializer(data_input_format):
     if data_input_format == 'json':
         return json.loads
+
+    if data_input_format == 'csv':
+        def unpack_csv(msg):
+            csv_file = StringIO(msg)
+            reader = csv.reader(csv_file)
+            headers = next(reader)
+            values = next(reader)
+            return dict(zip(headers, values))
+        return unpack_csv
 
 
 def get_logger(log_config_path='utils/logger_config.yml'):
